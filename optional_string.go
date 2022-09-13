538994c7b416e63c3c72d51055abb371d2efa57e
ngicks <yknt.bsl@gmail.com>
2022-09-13 11:03:37 +0000

    impl optional string parse - test passing

--- optional_string.go
+++ optional_string.go
@@ -1,17 +1,26 @@
 package flextime
 
 import (
+	"fmt"
+
 	"github.com/pkg/errors"
 	parsec "github.com/prataprc/goparsec"
 )
 
-func MakeOptionalStringParser(ast *parsec.AST) parsec.Parser {
-	opensqr := parsec.Atom(`[`, "OPENSQR")
-	closesqr := parsec.Atom(`]`, "CLOSESQR")
-	escapedchar := parsec.Token(`\\.`, "ESCAPEDCHAR")
-	normalchar := parsec.Token(`[^\[\]\\]+`, "NORMALCHAR")
+const (
+	OPENSQR     = "OPENSQR"
+	CLOSESQR    = "CLOSESQR"
+	ESCAPEDCHAR = "ESCAPEDCHAR"
+	NORMALCHARS = "NORMALCHARS"
+)
 
-	char := ast.OrdChoice("char", nil, escapedchar, normalchar)
+func MakeOptionalStringParser(ast *parsec.AST) parsec.Parser {
+	opensqr := parsec.Atom(`[`, OPENSQR)
+	closesqr := parsec.Atom(`]`, CLOSESQR)
+	escapedchar := parsec.Token(`\\.`, ESCAPEDCHAR)
+	normalchars := parsec.Token(`[^\[\]\\]+`, NORMALCHARS)
+
+	char := ast.OrdChoice("char", nil, escapedchar, normalchars)
 	chars := ast.Many("chars", nil, char)
 
 	var optional parsec.Parser
@@ -19,6 +28,19 @@ func MakeOptionalStringParser(ast *parsec.AST) parsec.Parser {
 	items := ast.Kleene("items", nil, item)
 	optional = ast.And("optional", nil, opensqr, items, closesqr)
 	return ast.Kleene("optionalString", nil, ast.OrdChoice("items", nil, optional, chars))
+}
+
+type SyntaxError struct {
+	org      string
+	parsedAs string
+}
+
+func (e SyntaxError) Error() string {
+	return fmt.Sprintf(
+		"syntax error: maybe no opening/closing sqrt? parsed result = %s, input = %s",
+		e.parsedAs,
+		e.org,
+	)
 }
 
 func EnumerateOptionalString(optionalString string) (enumerated []string, err error) {
@@ -30,8 +52,128 @@ func EnumerateOptionalString(optionalString string) (enumerated []string, err er
 
 	ast := parsec.NewAST("optionalString", 100)
 	p := MakeOptionalStringParser(ast)
-	s := parsec.NewScanner([]byte(optionalString)).TrackLineno()
-	ast.Parsewith(p, s)
+	s := parsec.NewScanner([]byte(optionalString))
+	node, _ := ast.Parsewith(p, s)
 
-	return []string{}, nil
+	if parsedAs := node.GetValue(); len(parsedAs) != len(optionalString) {
+		return []string{}, &SyntaxError{
+			org:      optionalString,
+			parsedAs: parsedAs,
+		}
+	}
+
+	root := optStrContext{
+		node: &treeNode{},
+	}
+	ctx := root
+	recursiveDecode(node, &ctx)
+
+	return root.Trunc(), nil
+}
+
+type optStrContext struct {
+	node *treeNode
+}
+
+func (ctx *optStrContext) Open() {
+	ctx.node.open = true
+}
+func (ctx *optStrContext) IsOpened() bool {
+	return ctx.node.open
+}
+func (ctx *optStrContext) Close() {
+	ctx.node.open = false
+}
+
+func (ctx *optStrContext) IsRoot() bool {
+	return ctx.node.parent == nil
+}
+
+func (ctx *optStrContext) StepOptional() *optStrContext {
+	if ctx.node.optional == nil {
+		ctx.node.optional = &treeNode{parent: ctx.node}
+	}
+	return &optStrContext{
+		node: ctx.node.optional,
+	}
+}
+
+func (ctx *optStrContext) StepNext() *optStrContext {
+	if ctx.node.next == nil {
+		ctx.node.next = &treeNode{parent: ctx.node}
+	}
+	return &optStrContext{
+		node: ctx.node.next,
+	}
+}
+
+func (ctx *optStrContext) StepBack() *optStrContext {
+	return &optStrContext{
+		node: ctx.node.parent,
+	}
+}
+
+func (ctx *optStrContext) AddString(str string) {
+	ctx.node.value += str
+}
+
+func (ctx *optStrContext) Trunc() []string {
+	return ctx.trunc()
+}
+
+func (ctx *optStrContext) trunc() []string {
+	cur := ctx.node.value
+	ret := []string{cur}
+
+	if ctx.node.optional != nil {
+		for _, s := range ctx.StepOptional().trunc() {
+			ret = append(ret, cur+s)
+		}
+	}
+
+	if ctx.node.next != nil {
+		nextStr := ctx.StepNext().trunc()
+		if len(nextStr) > 0 {
+			org := make([]string, len(ret))
+			copy(org, ret)
+			// org := ret
+			ret = ret[:0]
+			for _, nn := range nextStr {
+				for _, oo := range org {
+					ret = append(ret, oo+nn)
+				}
+			}
+		}
+	}
+
+	return ret
+}
+
+type treeNode struct {
+	parent   *treeNode
+	open     bool
+	value    string
+	optional *treeNode
+	next     *treeNode
+}
+
+func recursiveDecode(node parsec.Queryable, ctx *optStrContext) {
+	for _, child := range node.GetChildren() {
+		switch child.GetName() {
+		case OPENSQR:
+			if ctx.IsOpened() {
+				ctx = ctx.StepNext()
+			}
+			ctx.Open()
+			ctx = ctx.StepOptional()
+		case CLOSESQR:
+			ctx = ctx.StepBack()
+		case ESCAPEDCHAR, NORMALCHARS:
+			if ctx.IsOpened() {
+				ctx = ctx.StepNext()
+			}
+			ctx.AddString(child.GetValue())
+		}
+		recursiveDecode(child, ctx)
+	}
 }
--- optional_string_test.go
+++ optional_string_test.go
@@ -1,6 +1,7 @@
 package flextime_test
 
 import (
+	"fmt"
 	"sort"
 	"testing"
 
@@ -9,22 +10,80 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestYYYMMDDOptional(t *testing.T) {
-	input := `YYYY-MM-DD[THH[:mm[:ss.SSS]]][Z]`
-	output := []string{
-		`YYYY-MM-DDTHH:mm:ss.SSSZ`,
-		`YYYY-MM-DDTHH:mm:ss.SSS`,
-		`YYYY-MM-DDTHH:mmZ`,
-		`YYYY-MM-DDTHH:mm`,
-		`YYYY-MM-DDTHHZ`,
-		`YYYY-MM-DDTHH`,
-		`YYYY-MM-DDZ`,
-		`YYYY-MM-DD`,
+type variantsTestCases struct {
+	input  string
+	output []string
+}
+
+func TestMakeVariantsOptinalString(t *testing.T) {
+	cases := []variantsTestCases{
+		{
+			input: `[YYYY[-M]M]-DDTHH:mm:ss.SSSZ`,
+			output: []string{
+				`YYYY-MM-DDTHH:mm:ss.SSSZ`,
+				`YYYYM-DDTHH:mm:ss.SSSZ`,
+				`-DDTHH:mm:ss.SSSZ`,
+			},
+		},
+		{
+			input: `YYYY-MM-DDTHH[:mm[:ss.SSS]]`,
+			output: []string{
+				`YYYY-MM-DDTHH:mm:ss.SSS`,
+				`YYYY-MM-DDTHH:mm`,
+				`YYYY-MM-DDTHH`,
+			},
+		},
+		{
+			input: `YYYY-MM-DD[THH[:mm[:ss.SSS]]][Z]`,
+			output: []string{
+				`YYYY-MM-DDTHH:mm:ss.SSSZ`,
+				`YYYY-MM-DDTHH:mm:ss.SSS`,
+				`YYYY-MM-DDTHH:mmZ`,
+				`YYYY-MM-DDTHH:mm`,
+				`YYYY-MM-DDTHHZ`,
+				`YYYY-MM-DDTHH`,
+				`YYYY-MM-DDZ`,
+				`YYYY-MM-DD`,
+			},
+		},
+		{
+			input: `YYYY-MM-DD[THH[:mm[:ss.SSS]]]a[Z]`,
+			output: []string{
+				`YYYY-MM-DDTHH:mm:ss.SSSaZ`,
+				`YYYY-MM-DDTHH:mm:ss.SSSa`,
+				`YYYY-MM-DDTHH:mmaZ`,
+				`YYYY-MM-DDTHH:mma`,
+				`YYYY-MM-DDTHHaZ`,
+				`YYYY-MM-DDTHHa`,
+				`YYYY-MM-DDaZ`,
+				`YYYY-MM-DDa`,
+			},
+		},
 	}
 
-	result, err := flextime.EnumerateOptionalString(input)
-	require.NoError(t, err)
-	sort.Strings(result)
-	sort.Strings(output)
-	assert.Equal(t, output, result)
+	for _, testCase := range cases {
+		t.Run(fmt.Sprintf("case: %s", testCase.input), func(t *testing.T) {
+			result, err := flextime.EnumerateOptionalString(testCase.input)
+			require.NoError(t, err)
+			sort.Strings(result)
+			sort.Strings(testCase.output)
+			assert.Equal(t, testCase.output, result)
+		})
+	}
+
+}
+
+func TestOptionalNonClosing(t *testing.T) {
+	cases := []string{
+		`foobar[baz[qux[`,
+		`foobar\[baz[qux\[`,
+		`foobarbaz]qux[]`,
+		`foobarbaz\]qux[\]`,
+		`foobarbaz\]qux\[]`,
+	}
+
+	for _, input := range cases {
+		_, err := flextime.EnumerateOptionalString(input)
+		require.Error(t, err)
+	}
 }
