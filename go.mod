2709f6f4b52864b56866fc0c68268ccaaae8a805
ngicks <yknt.bsl@gmail.com>
2022-10-11 14:55:21 +0000

    refactor: delegates Must function

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/google/go-cmp v0.5.9
-	github.com/ngicks/type-param-common v0.0.14
+	github.com/ngicks/type-param-common v0.0.15
 	github.com/pkg/errors v0.9.1
 	github.com/prataprc/goparsec v0.0.0-20211219142520-daac0e635e7e
 	github.com/stretchr/testify v1.8.0
@@ -13,5 +13,6 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20220613132600-b0d781184e0d // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- predefined.go
+++ predefined.go
@@ -1,19 +1,13 @@
 package flextime
 
-import "time"
+import (
+	"time"
+
+	typeparamcommon "github.com/ngicks/type-param-common"
+)
 
 // RFC3339Optinal is LayoutSet where year, month, date is mandatory.
 // And lower parts (hours, minutes, seconds, nanoseconds) and timezone offset are optional.
-var RFC3339Optinal *LayoutSet = Must(
-	func() (*LayoutSet, error) { return NewLayoutSet(`YYYY-MM-DD[THH[:mm[:ss.999999999]]][Z]`) },
-)
+var RFC3339Optinal *LayoutSet = typeparamcommon.Must(NewLayoutSet(`YYYY-MM-DD[THH[:mm[:ss.999999999]]][Z]`))
 
 var RFC3339orUnixMilli *CombinedFlextime = NewCombined([]*Flextime{NewFlextime(RFC3339Optinal)}, time.UnixMilli)
-
-func Must[T any](newFunc func() (T, error)) T {
-	v, err := newFunc()
-	if err != nil {
-		panic(err)
-	}
-	return v
-}
